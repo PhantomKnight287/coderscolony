c124feb7e37dcf2600a86c8420c4a729ca40c3b3
phantomknight287 <gillgurpal699@gmail.com>
2022-10-30 19:23:48 +0530

    chore(frontend): setup `sass` and rewrites for backend proxy

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,22 @@
+const { join } = require("path");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  devIndicators: {
+    buildActivity: true,
+    buildActivityPosition: "top-right",
+  },
+  sassOptions: {
+    includePaths: [join(__dirname, "styles"), join(__dirname, "components")],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:slug*",
+        destination: `${process.env.API_URL}/:slug*`,
+      },
+    ];
+  },
+};
