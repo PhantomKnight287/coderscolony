c764af94029448b6434e6a86db60525b03da3923
phantomknight287 <gillgurpal699@gmail.com>
2022-10-30 19:26:15 +0530

    chore(backend):import `MetadataController`

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AuthController } from './routes/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 import { PrismaService } from './services/prisma/prisma.service';
+import { MetadataController } from './routes/metadata/metadata.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, AuthController],
+  controllers: [AppController, AuthController, MetadataController],
   providers: [AppService, AuthService, PrismaService],
 })
 export class AppModule {}
