c427a07fa4faa1428710e2addfb95745d3477cc5
phantomknight287 <gillgurpal699@gmail.com>
2022-10-30 19:21:49 +0530

    chore(frontend): create `user` hooks

---
+++ apps/frontend/hooks/user/index.ts
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { UserContext } from "../../context/user";
+
+export function useUser() {
+  const context = useContext(UserContext);
+  if (context === null) throw new Error("App not wrapped in `UserProvider`");
+  return context.user;
+}
+
+export function useUserDispatch() {
+  const context = useContext(UserContext);
+  if (context === null) throw new Error("App not wrapped in `UserProvider`");
+  return context.setUser;
+}
