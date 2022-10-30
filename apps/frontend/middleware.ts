5df807f04b0c191a58baf5847390c887180e5a79
phantomknight287 <gillgurpal699@gmail.com>
2022-10-30 19:24:05 +0530

    chore(frontend): create middleware(WIP)

---
+++ apps/frontend/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.cookies.get("token") === undefined) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+  return NextResponse.next();
+}
+export const config = {
+  matcher: [],
+};
