18ee35eac22c08ff1ef9331d21f04b62b56f395e
phantomknight287 <gillgurpal699@gmail.com>
2022-10-31 16:27:37 +0530

    chore(backend): implement dotenv config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as morgan from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import { config } from 'dotenv';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
+  app.use(cookieParser());
   app.use(morgan(':method :url :status :response-time ms'));
   await app.listen(5000);
 }
+config();
 bootstrap();
