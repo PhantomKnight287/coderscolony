edb2ea506369ec231b985ebac12ed1f9376584c5
phantomknight287 <gillgurpal699@gmail.com>
2022-10-31 16:31:34 +0530

    feat(frontend): new hook to hydrate context state

---
+++ apps/frontend/hooks/hydrate/context.ts
@@ -0,0 +1,38 @@
+import { readCookie } from "@helpers/cookies";
+import { useUserDispatch } from "@hooks/user";
+import axios from "axios";
+import { User } from "db";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export function useHydrateUserContext() {
+  const dispatch = useUserDispatch();
+  const { replace } = useRouter();
+  useEffect(() => {
+    const cookie = readCookie("token");
+    if (!cookie) return;
+    axios
+      .get<Exclude<User, "password">>("/api/auth/hydrate", {
+        headers: {
+          authorization: `Bearer ${cookie}`,
+        },
+      })
+      .then((d) => d.data)
+      .then((d) => {
+        dispatch({
+          type: "SetUser",
+          payload: d,
+        });
+      })
+      .catch((err) => {
+        const header = err.response?.headers["X-Error"];
+        if (!header) return;
+        if (header)
+          return replace(
+            `/auth/login?message=${encodeURIComponent(
+              "Session Expired, Please Login Again."
+            )}`
+          );
+      });
+  }, []);
+}
