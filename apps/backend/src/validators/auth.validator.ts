31e2a4cae304b9110b839c27b1378279c7db2540
phantomknight287 <gillgurpal699@gmail.com>
2022-10-31 16:29:43 +0530

    chore(backend): add new `name` property in Auth validator

--- apps/backend/src/validators/auth.validator.ts
+++ apps/backend/src/validators/auth.validator.ts
@@ -4,6 +4,7 @@ export const SignUp = JOI.object({
   email: JOI.string().email().required(),
   password: JOI.string().required(),
   username: JOI.string().alphanum().required(),
+  name: JOI.string().required(),
 });
 
 export const Login = JOI.object({
