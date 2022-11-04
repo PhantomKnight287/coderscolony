6e9ac9c597698b06647034941efdff4a4c9f2a65
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:28:55 +0530

    [frontend]: create hooks to mutate drawer state

---
+++ apps/frontend/hooks/drawer/index.ts
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { DrawerContext } from "../../context/drawer";
+
+export function useDrawer() {
+  const context = useContext(DrawerContext);
+  if (context === null)
+    throw new Error("Component Tree not wrapped in `DrawerProvider`");
+  return context;
+}
