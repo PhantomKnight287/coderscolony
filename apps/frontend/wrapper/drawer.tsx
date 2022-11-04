42b0d8a14911fc0d39dc96c59dc496400a94e961
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:31:26 +0530

    [frontend]: create `DrawerWrapper` component

---
+++ apps/frontend/wrapper/drawer.tsx
@@ -0,0 +1,21 @@
+import { useDrawer } from "@hooks/drawer";
+import { Drawer } from "@mantine/core";
+import { ReactNode } from "react";
+import styles from "./drawer.module.scss";
+
+export function DrawerWrapper(props: { children: ReactNode | ReactNode[] }) {
+  const { opened, setOpened } = useDrawer();
+
+  return (
+    <>
+      <Drawer
+        opened={opened}
+        className={styles.wrapper}
+        onClose={() => setOpened(false)}
+      >
+        {props.children}
+      </Drawer>
+      <div className={styles.contentWrapper}>{props.children}</div>
+    </>
+  );
+}
