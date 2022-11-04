d1cdc943b06f799f8673edb48bcce82d0bf391e5
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:19:27 +0530

    chore(backend): remove default logs of nestjs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as morgan from 'morgan';
-import * as cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
   app.enableCors();
   app.use(helmet());
-  app.use(cookieParser());
   app.use(morgan(':method :url :status :response-time ms'));
+  console.log('Server Starter');
   await app.listen(5000);
 }
 config();
