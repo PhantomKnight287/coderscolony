fb44df6951b3279a305055cec3d3f6ea365349a2
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:23:50 +0530

    refactor(frontend): upload `rewrite` and `includePaths`

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -9,13 +9,21 @@ module.exports = {
     buildActivityPosition: "top-right",
   },
   sassOptions: {
-    includePaths: [join(__dirname, "styles"), join(__dirname, "components")],
+    includePaths: [
+      join(__dirname, "styles"),
+      join(__dirname, "components"),
+      join(__dirname, "wrapper"),
+    ],
   },
   async rewrites() {
     return [
       {
         source: "/api/:slug*",
         destination: `${process.env.API_URL}/:slug*`,
+      },
+      {
+        source: "/images/:slug*",
+        destination: `${process.env.STORAGE_BUCKET_URL}/:slug*`,
       },
     ];
   },
