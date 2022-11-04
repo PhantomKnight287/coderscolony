c9980edf77ad7c5e2c88925440558ae6d34a0f5a
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:27:12 +0530

    [frontend]: create `Label` Component

---
+++ apps/frontend/components/label/index.tsx
@@ -0,0 +1,26 @@
+import { Text, TextProps } from "@mantine/core";
+import { FC, ReactNode } from "react";
+import styles from "./label.module.scss";
+
+const Label: FC<
+  { required?: boolean; children: ReactNode } & Partial<TextProps>
+> = ({ children, required }) => {
+  return (
+    <div>
+      <Text
+        size={14}
+        sx={{
+          fontWeight: "lighter",
+          marginTop: 12,
+          marginBottom: 3,
+          display: "inline",
+        }}
+      >
+        {children} {" "}
+      </Text>
+      {required ? <span className={styles.asterisk}>*</span> : null}
+    </div>
+  );
+};
+
+export default Label;
