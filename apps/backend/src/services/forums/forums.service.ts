b6bf61e3560fdb84fceda8d43cd61d0155f0785e
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:22:08 +0530

    chore(backend): create `ForumsService` to follow `DRY` principle

---
+++ apps/backend/src/services/forums/forums.service.ts
@@ -0,0 +1,46 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { slugify } from 'src/helpers/slugify';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ForumsService {
+  constructor(protected prisma: PrismaService) {}
+  async getForumInfo(slug: string) {
+    const forumInfo = await this.prisma.prisma.forums.findFirst({
+      where: {
+        urlSlug: slugify(slug),
+      },
+      select: {
+        bannerImage: true,
+        createdAt: true,
+        description: true,
+        name: true,
+        profileImage: true,
+        forumMembers: {
+          select: {
+            role: true,
+            user: {
+              select: {
+                username: true,
+                profileImage: true,
+                name: true,
+              },
+            },
+          },
+        },
+        urlSlug: true,
+      },
+    });
+    if (!forumInfo)
+      throw new HttpException(
+        'No Forum Found With Given Slug',
+        HttpStatus.NOT_FOUND,
+      );
+    return {
+      ...forumInfo,
+      forumMembers: forumInfo.forumMembers.length,
+      admins: forumInfo.forumMembers.filter((d) => d.role === 'ADMIN'),
+      moderators: forumInfo.forumMembers.filter((d) => d.role === 'MODERATOR'),
+    };
+  }
+}
