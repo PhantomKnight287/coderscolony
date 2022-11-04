8d18735996356e0c0f8736074889e28b7aedd4c2
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:26:18 +0530

    [backend]: add `services` and `controllers` to `@Module`

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -8,10 +8,13 @@ import { MetadataController } from './routes/metadata/metadata.controller';
 import { NotificationsGateway } from './gateways/notifications/notifications.gateway';
 import { NotificationsController } from './routes/notifications/notifications.controller';
 import { NotificationsService } from './services/notifications/notifications.service';
+import { ForumsController } from './routes/forums/forums.controller';
+import { UploadController } from './routes/upload/upload.controller';
+import { ForumsService } from './services/forums/forums.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, AuthController, MetadataController, NotificationsController],
-  providers: [AppService, AuthService, PrismaService, NotificationsGateway, NotificationsService],
+  controllers: [AppController, AuthController, MetadataController, NotificationsController, ForumsController, UploadController],
+  providers: [AppService, AuthService, PrismaService, NotificationsGateway, NotificationsService, ForumsService],
 })
 export class AppModule {}
