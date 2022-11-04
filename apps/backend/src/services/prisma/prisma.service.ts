16c0644646867536867c9149b9ddff88490edac3
phantomknight287 <gillgurpal699@gmail.com>
2022-11-04 20:22:29 +0530

    chore(backend): connect to backend in `constructor` of `PrismaService`

--- apps/backend/src/services/prisma/prisma.service.ts
+++ apps/backend/src/services/prisma/prisma.service.ts
@@ -5,4 +5,10 @@ export class PrismaService {
   prisma: ReturnType<typeof getClient> = getClient({
     dev: process.env.NODE_ENV === 'development',
   });
+  constructor() {
+    this.prisma
+      .$connect()
+      .then(() => console.log('Connected To Database'))
+      .catch((err) => console.log('Unable to Connect To database', err));
+  }
 }
