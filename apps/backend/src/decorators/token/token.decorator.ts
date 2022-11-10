7a679219ff0c5958cbe7f62384ea3fd1f53a7bb6
phantomknight287 <gillgurpal699@gmail.com>
2022-11-10 21:46:37 +0530

    [backend] add decoder for JWT in `@Token` decorator

--- apps/backend/src/decorators/token/token.decorator.ts
+++ apps/backend/src/decorators/token/token.decorator.ts
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-
 import {
   createParamDecorator,
   ExecutionContext,
@@ -7,9 +5,14 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { verify } from 'jsonwebtoken';
+import { DecodedJWT } from 'src/types/jwt';
 
 export const Token = createParamDecorator(
-  (data: { optional?: boolean }, ctx: ExecutionContext) => {
+  (
+    data: { optional?: boolean; validate?: boolean },
+    ctx: ExecutionContext,
+  ): string | DecodedJWT => {
     const request = ctx.switchToHttp().getRequest<Request>();
     const token = request.headers['authorization']?.replace('Bearer ', '');
     if (!token && !data?.optional)
@@ -17,6 +20,18 @@ export const Token = createParamDecorator(
         'Authentication Token is required to access this resource.',
         HttpStatus.UNAUTHORIZED,
       );
+    let jwt: DecodedJWT;
+    if (data?.validate) {
+      try {
+        jwt = verify(token, process.env.JWT_SECRET) as unknown as DecodedJWT;
+        return jwt;
+      } catch (err) {
+        throw new HttpException(
+          'Invalid or Expired Authentication Token',
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    }
     return token;
   },
 );
