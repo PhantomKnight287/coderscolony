f424a0ca5ae6c3149ecec9574a8ef1168d272db7
phantomknight287 <gillgurpal699@gmail.com>
2022-11-13 00:04:53 +0530

    chore: create `SlugPipe`

--- apps/backend/src/helpers/slugify.ts
+++ apps/backend/src/helpers/slugify.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 
 export const slugify = (str: string) =>
   str
---
+++ apps/backend/src/pipes/slug/slug.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  ArgumentMetadata,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { slugify } from 'src/helpers/slugify';
+
+@Injectable()
+export class SlugPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (!value)
+      throw new HttpException('No Slug Was supplied', HttpStatus.BAD_REQUEST);
+    return slugify(value);
+  }
+}
