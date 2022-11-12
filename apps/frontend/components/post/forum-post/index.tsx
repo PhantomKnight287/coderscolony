9607dce283fceb433b1449058f85edee1a90f0c2
phantomknight287 <gillgurpal699@gmail.com>
2022-11-13 00:10:02 +0530

    feat: create `ForumPost` component to render posts in a forum

---
+++ apps/frontend/components/post/forum-post/index.tsx
@@ -0,0 +1,30 @@
+import { Renderer } from "@components/renderer";
+import { getMarkdownString } from "@helpers/showdown";
+import { Paper } from "@mantine/core";
+import { useRouter } from "next/router";
+import { memo } from "react";
+import type { ForumPost } from "../../../types/forum-post";
+import { PostAuthor } from "./post-author";
+import styles from "./styles.module.scss";
+
+function ForumPost({ author, content, createdAt, id, slug }: ForumPost) {
+  const { query, push } = useRouter();
+
+  return (
+    <Paper
+      withBorder
+      mt="xl"
+      pt="md"
+      p="md"
+      className="cursor-pointer"
+      onClick={() => push(`/f/${query.name}/post/${slug}`)}
+    >
+      <PostAuthor {...author} createdAt={createdAt} />
+      <div className="ml-10">
+        <Renderer>{getMarkdownString(content)}</Renderer>
+      </div>
+    </Paper>
+  );
+}
+
+export default memo(ForumPost);
