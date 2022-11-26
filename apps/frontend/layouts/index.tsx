a5ad96f399aca408d90e1d49eee6d41fe00f57b3
phantomknight287 <gillgurpal699@gmail.com>
2022-11-26 21:51:42 +0530

    fix: showing sidebar

--- apps/frontend/layouts/index.tsx
+++ apps/frontend/layouts/index.tsx
@@ -1,17 +1,27 @@
 import { Sidebar } from "@components/navigation/sidebar";
 import { useSidebar } from "@hooks/sidebar";
-import { useUser } from "@hooks/user";
-import clsx from "clsx";
-import { FC, PropsWithChildren } from "react";
+import { useRouter } from "next/router";
+import { FC, PropsWithChildren, useEffect, useState } from "react";
 import styles from "./layout.module.scss";
 
 const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
-  const { id } = useUser();
+  // const { id } = useUser();
   const { opened } = useSidebar();
-  if (!id) return <>{children}</>;
+  // if (!id) return <>{children}</>;
+  const { asPath } = useRouter();
+  const [addClass, setAddClass] = useState(true);
+
+  useEffect(() => {
+    if (/\/u\/.*/.test(asPath)) {
+      setAddClass(false);
+    } else {
+      setAddClass(true);
+    }
+  }, [asPath]);
+
   return (
     <>
-      <div className={styles.container}>
+      <div className={addClass ? styles.container : ""}>
         {opened ? (
           <div className={styles.sidebar}>
             <Sidebar />
