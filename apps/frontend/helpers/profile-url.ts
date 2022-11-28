a411d3bf3a9916b6fd4fd9dd1702be12fc0bd618
phantomknight287 <gillgurpal699@gmail.com>
2022-11-28 21:41:25 +0530

    [frontend} create `profileImageResolver` to resolve profile image urls`

---
+++ apps/frontend/helpers/profile-url.ts
@@ -0,0 +1,13 @@
+export const profileImageResolver = ({
+  profileURL,
+  username,
+}: {
+  profileURL: string;
+  username: string;
+}) => {
+  return profileURL
+    ? profileURL.startsWith("https://avatar.dicebar")
+      ? profileURL
+      : `/images/${profileURL}`
+    : `https://avatars.dicebear.com/api/big-smile/${username}.svg`;
+};
