0ad20e8b75eeb95ad3a3a43abcc008da43dfc48f
phantomknight287 <gillgurpal699@gmail.com>
2022-11-28 21:40:14 +0530

    feat: create route to fetch followers(WIP)

--- apps/backend/src/routes/profile/profile.controller.ts
+++ apps/backend/src/routes/profile/profile.controller.ts
@@ -4,6 +4,7 @@ import {
   HttpException,
   HttpStatus,
   Param,
+  Query,
 } from '@nestjs/common';
 import { verify } from 'jsonwebtoken';
 import { Token } from 'src/decorators/token/token.decorator';
@@ -63,5 +64,45 @@ export class ProfileController {
       profile['edit'] = true;
     }
     return profile;
+  }
+  @Get(':username/network/followers')
+  async fetchUserNetwork(
+    // @Token({ optional: true }) token: string,
+    @Query('take') take: string,
+    @Param('username') username: string,
+  ) {
+    const _take = Number.isNaN(parseInt(take)) ? 5 : parseInt(take);
+    const user = await this.prisma.prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: 'insensitive',
+        },
+      },
+    });
+    if (!user) throw new HttpException('No User Found', HttpStatus.NOT_FOUND);
+    const followers = await this.prisma.prisma.follows.findMany({
+      where: {
+        following: {
+          id: user.id,
+        },
+      },
+      select: {
+        follower: {
+          select: {
+            username: true,
+            profileImage: true,
+            verified: true,
+            oneLiner: true,
+          },
+        },
+      },
+
+      take: _take,
+      skip: _take > 5 ? _take - 5 : undefined,
+    });
+    const res: Record<any, any> = { followers };
+    if (followers.length > 5) res['next'] = take + 5;
+    return res;
   }
 }
