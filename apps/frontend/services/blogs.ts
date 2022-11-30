ad5be2aad104662c93417a32429a904ddd38b2d7
phantomknight287 <gillgurpal699@gmail.com>
2022-11-30 21:27:06 +0530

    [frontend] create createBlog helper

---
+++ apps/frontend/services/blogs.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+interface CreateBlogProps {
+  title: string;
+  content: string;
+  tags?: string[];
+  ogImage?: string;
+  token: string;
+  description: string;
+}
+export async function createBlog(props: CreateBlogProps) {
+  const { content, title, token, ogImage, tags,description } = props;
+
+  return axios.post<{ slug: string }>(
+    "/api/blogs/create",
+    {
+      title,
+      content,
+      ogImage,
+      tags: tags || [],
+      description
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+}
