fc52446d92e773121572af865382a63ac97a6a10
phantomknight287 <gillgurpal699@gmail.com>
2022-11-30 21:25:08 +0530

    [frontend] create Blogs tab

--- apps/frontend/components/profile/blog.tsx
+++ apps/frontend/components/profile/blog.tsx
@@ -1,7 +1,79 @@
+import { BlogPost } from "@components/blog/item";
+import { SimpleGrid } from "@mantine/core";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Blog } from "db";
+import { useRouter } from "next/router";
+import { Fragment, useEffect } from "react";
+
 export function Blogs() {
-  return (
+  const { query, isReady } = useRouter();
+
+  const fetcher = async ({ pageParams = 0 }) => {
+    const res = await fetch(
+      `/api/blogs/${query.username}?take=${pageParams || 5}`
+    );
+    return res.json();
+  };
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery<{
+    blogs: Array<
+      Partial<Blog> & {
+        author: { username: string; id: string; profileImage: string };
+      }
+    >;
+    next?: number;
+  }>(["blogs"], (d) => fetcher({ pageParams: d.pageParam }), {
+    getNextPageParam: (lastPage, pages) => lastPage.next,
+    enabled: isReady,
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, []);
+
+  return status === "loading" ? (
+    <p>Loading...</p>
+  ) : status === "error" ? (
+    <p>Error: {(error as Error).message}</p>
+  ) : (
     <>
-      No Blogs Found
+      {data.pages.map((group, i) => (
+        <SimpleGrid
+          cols={3}
+          spacing="md"
+          my="xl"
+          key={i}
+          breakpoints={[
+            { maxWidth: "md", cols: 3, spacing: "md" },
+            { maxWidth: "sm", cols: 2, spacing: "sm" },
+            { maxWidth: "xs", cols: 1, spacing: "sm" },
+          ]}
+        >
+          {group.blogs?.map((blog) => (
+            <BlogPost {...blog} key={blog.id} />
+          ))}
+        </SimpleGrid>
+      ))}
+      <div>
+        <button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </button>
+      </div>
+      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
     </>
   );
 }
