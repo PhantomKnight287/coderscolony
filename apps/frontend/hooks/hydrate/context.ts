f4f9cf5cca76f85e798d2becf4b3c8c626dd2d49
phantomknight287 <gillgurpal699@gmail.com>
2022-12-01 21:45:11 +0530

    [frontend] return fetcher function from `useHydrateUserContext` hook

--- apps/frontend/hooks/hydrate/context.ts
+++ apps/frontend/hooks/hydrate/context.ts
@@ -8,7 +8,8 @@ import { useEffect } from "react";
 export function useHydrateUserContext() {
   const dispatch = useUserDispatch();
   const { replace } = useRouter();
-  useEffect(() => {
+
+  function fetchProfile() {
     const cookie = readCookie("token");
     if (!cookie) return;
     axios
@@ -34,5 +35,10 @@ export function useHydrateUserContext() {
             )}`
           );
       });
+  }
+
+  useEffect(() => {
+    fetchProfile();
   }, []);
+  return fetchProfile;
 }
