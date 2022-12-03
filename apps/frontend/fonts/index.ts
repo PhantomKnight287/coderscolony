de27b77a11646d4733c99c4c92e162b5105915ed
phantomknight287 <gillgurpal699@gmail.com>
2022-12-03 23:51:30 +0530

    [frontend] export fonts from `@next/font`

---
+++ apps/frontend/fonts/index.ts
@@ -0,0 +1,9 @@
+import { Inter, Space_Grotesk } from "@next/font/google";
+
+export const inter = Inter({
+  variable: "--font-inter",
+});
+
+export const spaceGrotest = Space_Grotesk({
+  variable: "--font-space-grotest",
+});
