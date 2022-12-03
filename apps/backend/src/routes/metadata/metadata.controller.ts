a1ded31acaeb3a2aa6f4bb9eb60c492ab9349226
phantomknight287 <gillgurpal699@gmail.com>
2022-12-03 23:42:28 +0530

    [backend] remove useless imports

--- apps/backend/src/routes/metadata/metadata.controller.ts
+++ apps/backend/src/routes/metadata/metadata.controller.ts
@@ -4,22 +4,10 @@ import {
   HttpException,
   HttpStatus,
   Post,
-  Request,
-  Response,
 } from '@nestjs/common';
-import { Response as R, Request as RQ } from 'express';
-import { getUrlMetadata } from 'src/helpers/metadata';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const getMetaData = require('metadata-scraper');
 
-const errorMessages = {
-  badPath:
-    'This is not a valid path for this service.  Please refer to the documentation: https://github.com/mozilla/page-metadata-service#url-metadata',
-  headerRequired: 'The content-type header must be set to application/json.',
-  urlsRequired:
-    'The post body must be a JSON payload in the following format: {urls: ["http://example.com"]}.',
-  maxUrls: 'A maximum of 20 urls can be sent for processing in one call.',
-};
 @Controller('metadata')
 export class MetadataController {
   @Post('/')
