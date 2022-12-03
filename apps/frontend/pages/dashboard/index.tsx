353a995a45d3c5b496a36442a34811d0210ae04d
phantomknight287 <gillgurpal699@gmail.com>
2022-12-03 23:53:59 +0530

    [frontend] refactor

--- apps/frontend/pages/dashboard/index.tsx
+++ apps/frontend/pages/dashboard/index.tsx
@@ -1,10 +1,16 @@
 import { useHydrateUserContext } from "@hooks/hydrate/context";
+import { useSidebar } from "@hooks/sidebar";
 import { Container } from "@mantine/core";
 import { useEffect, useRef } from "react";
 
 function Dashboard() {
   useHydrateUserContext();
   const ref = useRef<HTMLVideoElement>(null);
+  const { opened, setOpened } = useSidebar();
+
+  useEffect(() => {
+    if (opened === false) return setOpened(true);
+  }, [opened]);
 
   return (
     <>
