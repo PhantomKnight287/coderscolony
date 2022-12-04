64fc6329f48142fe2da73c688df729e2aec52202
phantomknight287 <gillgurpal699@gmail.com>
2022-12-04 23:01:11 +0530

    [db] remove `description` from `Forums` field

---
+++ packages/db/prisma/migrations/20221204135608_remove_description_field_from_forusm_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Forums` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Forums" DROP COLUMN "description";
