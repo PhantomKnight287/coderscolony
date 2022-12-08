0503c7ce83f7d8c318d75c7f82e0f9d2fea915a0
phantomknight287 <gillgurpal699@gmail.com>
2022-12-09 00:19:12 +0530

    [frontend] remove google fonts link tag

--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -10,19 +10,6 @@ export default class _Document extends Document {
 		return (
 			<Html>
 				<Head>
-					<link
-						rel="preconnect"
-						href="https://fonts.googleapis.com"
-					/>
-					<link
-						rel="preconnect"
-						href="https://fonts.gstatic.com"
-						crossOrigin=""
-					/>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
-						rel="stylesheet"
-					/>
 				</Head>
 				<body>
 					<Main />
