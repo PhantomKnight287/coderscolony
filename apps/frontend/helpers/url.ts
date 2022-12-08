62c9b41e383659fee8c330070d577605abff9883
phantomknight287 <gillgurpal699@gmail.com>
2022-12-08 19:29:09 +0530

    [frontend] use regex to check if string is valid url

--- apps/frontend/helpers/url.ts
+++ apps/frontend/helpers/url.ts
@@ -1,9 +1,12 @@
 export const isValidURL = (url: string) => {
-	let url_;
-	try {
-		url_ = new URL(url);
-	} catch {
-		return false;
-	}
-	return url_.protocol === "http:" || url_.protocol === "https:";
+	const pattern = new RegExp(
+		"^(https?:\\/\\/)?" + // protocol
+			"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+			"((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+			"(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+			"(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+			"(\\#[-a-z\\d_]*)?$",
+		"i"
+	); // fragment locator
+	return !!pattern.test(url);
 };
