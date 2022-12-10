327ac7f09af9f6032f35c72a72b658e0cb0ece15
phantomknight287 <gillgurpal699@gmail.com>
2022-12-10 13:16:13 +0530

    [backend] refactor Container Component

--- apps/frontend/components/container/index.tsx
+++ apps/frontend/components/container/index.tsx
@@ -2,7 +2,7 @@ import { Container as C, ContainerProps } from "@mantine/core";
 
 export function Container(props: Partial<ContainerProps>) {
 	return (
-		<C {...props} className={`${props.className} mt-20`}>
+		<C {...props} className={`mt-20 ${props.className}`}>
 			{props.children}
 		</C>
 	);
