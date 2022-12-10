e95d0049b4a0572004cc2f4b95b31ad868f1ab17
phantomknight287 <gillgurpal699@gmail.com>
2022-12-10 23:11:25 +0530

    [backend] mark day as optional

--- apps/frontend/helpers/cookies.ts
+++ apps/frontend/helpers/cookies.ts
@@ -3,7 +3,7 @@
 createCookie("myCookieUniqueName", value, 30);
 createCookie("myJsonCookieUniqueName", json, 30);
 */
-export function createCookie(name: string, value: string, days: number) {
+export function createCookie(name: string, value: string, days?: number) {
 	if (days) {
 		var date = new Date();
 		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
