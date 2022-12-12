47a7a5b61fc51395d8f044899b9cf36558a04d50
phantomknight287 <gillgurpal699@gmail.com>
2022-12-12 23:24:18 +0530

    [frontend] set default days to 60 for cookies

--- apps/frontend/helpers/cookies.ts
+++ apps/frontend/helpers/cookies.ts
@@ -4,11 +4,10 @@ createCookie("myCookieUniqueName", value, 30);
 createCookie("myJsonCookieUniqueName", json, 30);
 */
 export function createCookie(name: string, value: string, days?: number) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-		var expires = "; expires=" + date.toUTCString();
-	} else var expires = "";
+	days = days || 60;
+	var date = new Date();
+	date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+	var expires = "; expires=" + date.toUTCString();
 	document.cookie =
 		name + "=" + value + expires + "; path=/;SameSite=None;Secure";
 }
