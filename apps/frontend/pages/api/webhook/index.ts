c7988dc24a65f99d1da84d34984d4306525ed1b9
phantomknight287 <gillgurpal699@gmail.com>
2022-12-12 23:25:23 +0530

    [frontend] add `return` to webhook route

--- apps/frontend/pages/api/webhook/index.ts
+++ apps/frontend/pages/api/webhook/index.ts
@@ -2,15 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method !== "POST") {
-		res.status(405).end("Method not allowed");
+		return res.status(405).end("Method not allowed");
 	}
 	// check if secret provided is correct
 	if (req.body.secret !== process.env.SECRET) {
-		res.status(401).end("Unauthorized");
+		return res.status(401).end("Unauthorized");
 	}
 	// get the "route" from the request body and ISR it
 	const { route } = req.body;
 
 	res.revalidate(route);
-	res.send("ok");
+	return res.send("ok");
 }
